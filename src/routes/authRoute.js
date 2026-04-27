4a6393ef152436ef868bbcbccc4a0a1894d8a216
@ZedDaProgrammer <alexzedrick07@gmail.com>
2026-04-27 21:15:18 +0800

    Middleware

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { registration, loginUser } = require('../controllers/authController');
+const token = require('../middleware/authMiddleware');
 
 router.get('/test', (req, res) => {
     res.send('The auth route map is working perfectly!');
@@ -8,5 +9,13 @@ router.get('/test', (req, res) => {
 
 router.post('/login', loginUser);
 router.post('/register', registration);
+
+router.get('/profile', token, (req, res) =>{
+    res.json({
+        message: "Welcome to your profile",
+        user: req.user
+
+    });
+});
 
 module.exports = router;
