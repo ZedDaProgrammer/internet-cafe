29f15941ededbbb4943faa7a44ee239cd8489251
ZeD <alexzedrick07@gmail.com>
2026-04-28 19:19:39 +0800

    Register booking routes in server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const pool = require('./src/config/db');
 const authRoutes = require('./src/routes/authRoute');
 const computerRoutes = require('./src/routes/computerRoute');
+const bookingRoutes = require('./src/routes/bookingRoute');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/computers', computerRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 //route
 app.get('/', (req, res) => {
